9f1f40d5c63e908167cf539f97bf99c3a199e8d2
كارل مبارك <bonjour@mouabrak.eu>
2021-04-29 14:47:15 +0200

    cors and xframe

--- dev/back/config/middleware.js
+++ dev/back/config/middleware.js
@@ -3,8 +3,8 @@ module.exports = {
   settings: {
     cors: {
       enabled: true,
-      headers: '*',
-      // origin: ["http://localhost", 'https://obfsucario'],
+      // headers: '*',
+      origin: ["http://localhost", "https://3rd.obfuscationworkshop.org", "https://obfuscation.karls.computeR"],
     },
     xframe: {
       enabled: false,
