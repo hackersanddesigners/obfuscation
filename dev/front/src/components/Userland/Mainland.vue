5d39fbecbd960eaac6531cd6dacdb7e327638a6d
كارل مبارك <bonjour@mouabrak.eu>
2021-04-29 10:52:13 +0200

    fixed typi!

--- dev/front/src/components/Userland/Mainland.vue
+++ dev/front/src/components/Userland/Mainland.vue
@@ -425,7 +425,7 @@ export default {
     })
 
     window.onbeforeunload = () => {
-      if (this.me && this.me.delete === false) {
+      if (this.me && this.me.deleted === false) {
         this.$store.dispatch('disconnect')
         if (this.isMobile || this.me.name.includes(this.me.uid)) {
           this.$store.dispatch('deleteUser', this.me)
